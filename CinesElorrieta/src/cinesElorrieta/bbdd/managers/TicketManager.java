07b2be03a22e00f61932a20096c3c21d2c6863f2
corderaa <ugaitzcordero1@gmail.com>
2024-01-28 16:53:29 +0100

    Ahora si, añadidos los managers que faltaban

---
+++ CinesElorrieta/src/cinesElorrieta/bbdd/managers/TicketManager.java
@@ -0,0 +1,30 @@
+package cinesElorrieta.bbdd.managers;
+
+import java.util.List;
+
+import cinesElorrieta.bbdd.pojo.Ticket;
+
+/**
+ * Manager of the table t_Ticket. It contains all the methods for that table.
+ */
+public class TicketManager {
+
+	/**
+	 * Gets the Ticket specified by the id
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public Ticket getTicket(int id) {
+		return null;
+	}
+
+	/**
+	 * Gets all the Tickets from the table t_ticket. Returns a LIST of Tickets.
+	 * 
+	 * @return
+	 */
+	public List<Ticket> getAllTicket() {
+		return null;
+	}
+}
