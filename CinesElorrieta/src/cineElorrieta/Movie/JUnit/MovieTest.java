1df7adbcba92d9a68daf212666eec7feb7542a25
dennis20012001 <dennishm2001@gmail.com>
2024-01-29 21:23:22 +0100

    Empiezo de JUnit
    -Habra que hacer nulls de los casos de prueba.

---
+++ CinesElorrieta/src/cineElorrieta/Movie/JUnit/MovieTest.java
@@ -0,0 +1,17 @@
+package cineElorrieta.Movie.JUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import cinesElorrieta.bbdd.pojo.Movie;
+
+class MovieTest {
+
+	@Test
+	public void setMovieId (int movieId) {
+		   Movie movie = new Movie();
+		  // int resultado = Movie.setMovieId("1");
+		   //assertEquals(1, resultado);
+		}
+}
